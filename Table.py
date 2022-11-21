6ea46a606dcb1e33fd132305e474eb17e4bcce0f
xhxushush <116039975+xhxushush@users.noreply.github.com>
2022-11-21 18:18:15 -0500

    Add files via upload

---
+++ Table.py
@@ -0,0 +1,5 @@
+print("This program is for multiplication tables! ")
+n=int(input("Enter the number to print out tables for: "))
+
+for i in range(1,11): #For loop for 10 interations
+    print(n," x ",i," = ",i*n) #Printing out the table
